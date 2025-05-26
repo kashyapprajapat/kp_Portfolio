52f8ef25feff46d2bcbdfad2039a1cefa480a4f8
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-05-26 09:51:16 +0530

    Feat:Complet the Conatct us section with the help of emial js service.

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,9 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("nVp4QRNY0jl-RRdJc");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_placlg8', 'template_fjeugwq', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
